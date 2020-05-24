092796a8ebcc1f6a7787f69d0b0251677fdb074e
lucascr91 <lucas.ecomg@gmail.com>
2020-05-23 22:37:10 -0300

    update setup

--- cartpy/__init__.py
+++ cartpy/__init__.py
@@ -1,1 +1,2 @@
+from cartpy.data_path import *
 from cartpy.cartpy import Municipio
---
+++ cartpy/data_path.py
@@ -0,0 +1,2 @@
+from pkg_resources import resource_filename 
+open(resource_filename('data', 'counties_1872_1991.csv'), 'rb')
